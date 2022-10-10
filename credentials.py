003d8743801547a59bde92173d2553a7176199b3
deusopus <deusopus@gmail.com>
2022-10-10 07:29:20 -0700

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,5 @@
+insta_username = "<your username>"
+insta_password = "<your password>"
+
+hashtags = ["love", "friends", "food", "travel", "shopping"]
+captions = ["Wow!", "Amazing!", "Congrats!", "Cheers!", "Nice Shot!"]
