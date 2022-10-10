a451fa73b7b1ea206c95a39e3e9b932a324e21b8
deusopus <deusopus@gmail.com>
2022-10-10 14:04:32 -0700

    Update insta-bot.py

--- insta-bot.py
+++ insta-bot.py
@@ -1,31 +1,48 @@
 import random
 from time import sleep
-from instagrapi import *
-from credentials import *
+from instagrapi import Client
+from cred import *
 
 client = Client()
 
-tfa = input('Enter 2FA code: ')
-client.login(insta_username, insta_password, verification_code=tfa)
+class StartupRoutine:
+    def __init__(self, welcome, insta_username, insta_password, tfa, hashtag, caption, pause):
+        self.welcome = welcome
+        self.insta_username = insta_username
+        self.insta_password = insta_password
+        self.tfa = tfa
+        self.hashtag = hashtag
+        self.caption = caption
+        self.pause = pause
 
-while True:
-    hashtag = random.choice(hashtags)
-    caption = random.choice(captions)
-    wait = random.randrange(600,1200)
+startUp = StartupRoutine(print("Instagrapi Bot v.0.0.1 by deusopus@gmail.com"), insta_username, insta_password, input("Enter your 2FA code: "), random.choice(hashtags), random.choice(captions), random.randrange(2400, 4800))
 
-    target_post = client.hashtag_medias_recent(hashtag)
-    for i, media in enumerate(target_post):
+startUp.welcome
+startUp.tfa
+
+client.login(startUp.insta_username, startUp.insta_password, verification_code=startUp.tfa)
+
+posts = client.hashtag_medias_recent(startUp.hashtag)
+
+for i, media in enumerate(posts):
+        
         client.media_like(media.id)
-        print(f"Liked post {i+1} with #{hashtag} by @{media.user.username}")
+        print(f"Liked post {i+1} with #{startUp.hashtag} by @{media.user.username}")
+        wait = startUp.pause
         print(f"Sleeping for {wait} seconds")
         sleep(wait)
+        
         if i % 5 == 0 and i % 3 == 0:
+            
             client.user_follow(media.user.pk)
             print(f"Followed @{media.user.username}")
+            wait = startUp.pause
             print(f"Sleeping for {wait} seconds")
             sleep(wait)
-            
+
+            caption = startUp.caption
             client.media_comment(media.id,caption)
             print(f"Commented with: {caption} on post {i+1}")
+            wait = startUp.pause
             print(f"Sleeping for {wait} seconds")
             sleep(wait)
